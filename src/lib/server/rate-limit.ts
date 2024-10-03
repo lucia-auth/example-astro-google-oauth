9c234a661579b8204bce9bde3c994f468b24bbcb
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2024-10-04 00:16:35 +0900

    update rate limit

--- src/lib/server/rate-limit.ts
+++ src/lib/server/rate-limit.ts
@@ -1,4 +1,4 @@
-export class ConstantRefillTokenBucket<_Key> {
+export class TokenBucket<_Key> {
 	public max: number;
 	public refillIntervalSeconds: number;
 
@@ -10,6 +10,19 @@ export class ConstantRefillTokenBucket<_Key> {
 	private storage = new Map<_Key, Bucket>();
 
 	public check(key: _Key, cost: number): boolean {
+		const bucket = this.storage.get(key) ?? null;
+		if (bucket === null) {
+			return true;
+		}
+		const now = Date.now();
+		const refill = Math.floor((now - bucket.refilledAt) / (this.refillIntervalSeconds * 1000));
+		if (refill > 0) {
+			return Math.min(bucket.count + refill, this.max) >= cost;
+		}
+		return bucket.count >= cost;
+	}
+
+	public consume(key: _Key, cost: number): boolean {
 		let bucket = this.storage.get(key) ?? null;
 		const now = Date.now();
 		if (bucket === null) {
@@ -34,7 +47,6 @@ export class ConstantRefillTokenBucket<_Key> {
 		return true;
 	}
 }
-
 interface Bucket {
 	count: number;
 	refilledAt: number;
